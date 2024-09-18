af6d5cee7b92be35c98078bfb86e78024494d67b
pewillia <Peter.Williams@Canada.ca>
2024-09-18 10:59:56 -0400

    change readme instructions to run build and run preview
    (10)

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: "/geoview-demo/",
+ 
   resolve: {
     alias: {
       '@': '/src',
   },
-},
+  }
 })  
